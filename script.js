b8411b6cbc50a064972a525a2e8997015920b466
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-07-10 15:16:47 -0300

    definir direções e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -20,7 +20,24 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+
+document.addEventListener('keydown', update);/*captura o movimento da seta é chama a função update *//*também passa como argumento o evento */
+
+
+function update (event) {/* o evento é o movimento de tela */
+   if(event.keyCode == 37 && direction != "right") direction = "left";
+   if(event.keyCode == 38 && direction != "down") direction = "up";
+   if(event.keyCode == 39 && direction != "left") direction = "right";
+   if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;/*faz a cobrinha aparecer no outro lado da tela  */
+    if (snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
     criarBG();
     criarCobrinha();
 
